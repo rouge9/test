8a52fcb8f9368672cc97dbc0c19102daea0856d9
rouge9 <robelged1992@gmail.com>
2024-12-17 02:45:28 -0900

    add endpoint

--- frontend/src/queries/api.ts
+++ frontend/src/queries/api.ts
@@ -1,1 +1,2 @@
-export const VITE_BACKEND_API = "http://localhost:5000/api";
+// export const VITE_BACKEND_API = "http://localhost:5000/api";
+export const VITE_BACKEND_API = "https://test-x8n2.onrender.com";
