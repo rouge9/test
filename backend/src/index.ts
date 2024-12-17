6d965d561a91c4fa5c30d3893aa8db27741053bb
rouge9 <robelged1992@gmail.com>
2024-12-17 01:35:28 -0900

    setup for deloyment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,12 +9,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 app.use(express.json());
 
 // Serve frontend static files
@@ -31,7 +28,7 @@ app.get("*", (req, res) => {
 
 connectDB()
   .then(() => {
-    app.listen(5000, () => {
+    app.listen(PORT, () => {
       console.log("Server is running on port 5000");
     });
   })
